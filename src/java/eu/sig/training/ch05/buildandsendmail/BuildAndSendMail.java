2346e511d40ad1714863612f6ca77a5701a931d4
Eddo Careera Iriyanto Putra <eddocareera.i.p@gmail.com>
2017-05-16 10:47:12 +0700

    s

--- src/java/eu/sig/training/ch05/buildandsendmail/BuildAndSendMail.java
+++ src/java/eu/sig/training/ch05/buildandsendmail/BuildAndSendMail.java
@@ -1,5 +1,9 @@
 package eu.sig.training.ch05.buildandsendmail;
 
+import eu.sig.training.ch05.buildandsendmail.BuildAndSendMail.Employee;
+import eu.sig.training.ch05.buildandsendmail.BuildAndSendMail.Mail;
+import eu.sig.training.ch05.buildandsendmail.BuildAndSendMail.Message;
+
 public class BuildAndSendMail {
     public static class Employee {
 		public String firstName;
@@ -13,17 +17,50 @@ public class BuildAndSendMail {
 		}
 	}
 
+	public static class Message {
+		public String message1;
+		public String message2;
+		public String message3;
+
+		public Message(String message1, String message2, String message3) {
+			this.message1 = message1;
+			this.message2 = message2;
+			this.message3 = message3;
+		}
+	}
+
+	public static class Mail {
+		public String subject;
+		public MailFont font;
+		public Message message;
+
+		public Mail(String subject, MailFont font, Message message) {
+			this.subject = subject;
+			this.font = font;
+			this.message = message;
+		}
+	}
+
+	public static class Email {
+		public Employee employee;
+		public Mail mail;
+
+		public Email(Employee employee, Mail mail) {
+			this.employee = employee;
+			this.mail = mail;
+		}
+	}
+
 	// tag::buildAndSendMail[]
-    public void buildAndSendMail(MailMan m, Employee employee, String subject,
-        MailFont font, String message1, String message2, String message3) {
+    public void buildAndSendMail(MailMan m, Email email) {
         // Format the email address
-        String mId = employee.firstName.charAt(0) + "." + employee.lastName.substring(0, 7) + "@"
-            + employee.division.substring(0, 5) + ".compa.ny";
+        String mId = email.employee.firstName.charAt(0) + "." + email.employee.lastName.substring(0, 7) + "@"
+            + email.employee.division.substring(0, 5) + ".compa.ny";
         // Format the message given the content type and raw message
-        MailMessage mMessage = formatMessage(font,
-            message1 + message2 + message3);
+        MailMessage mMessage = formatMessage(email.mail.font,
+            email.mail.message.message1 + email.mail.message.message2 + email.mail.message.message3);
         // Send message
-        m.send(mId, subject, mMessage);
+        m.send(mId, email.mail.subject, mMessage);
     }
     // end::buildAndSendMail[]
 
